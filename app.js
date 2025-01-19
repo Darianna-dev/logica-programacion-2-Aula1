35f2533553e727095f88a3fddb8dbeb53268ab74
Darianna-dev <dariannagallo@gmail.com>
2025-01-19 13:32:49 +0000

    Primer commit del Juego para Adivinar un número de la práctica 2

---
+++ app.js
@@ -0,0 +1,10 @@
+const titulo = document.querySelector("h1");
+const parrafo = document.querySelector(".texto__parrafo");
+
+titulo.innerHTML = "Juego del Número Secreto";
+
+parrafo.innerHTML = "Escoge un número del 1 al 10";
+
+function intentoDeUsuario() {
+    alert("Click desde el boton");
+}
